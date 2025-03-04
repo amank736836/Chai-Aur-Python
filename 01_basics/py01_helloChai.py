b14f9af6fb6b2eb5b5bb4da19c238eb6cfdc895f
Aman Kumar <amankarguwal0@gmail.com>
2025-03-04 07:45:55 +0530

    add initial Python script to demonstrate chai variations

---
+++ 01_basics/py01_helloChai.py
@@ -0,0 +1,20 @@
+print("Chai Aur Python - Aman")
+
+
+def chai(n):
+    print(n)
+
+
+chai(4)
+chai("lemon tea")
+
+chai_one = "lemon tea"
+chai_two = "ginger tea"
+chai_three = "masala tea"
+chai_four = "black tea"
+chai_five = "green tea"
+
+# from importlib import reload
+# import sys
+# import os
+
